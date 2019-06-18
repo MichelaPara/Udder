de7ab4d8a0293f0e8ec73d4de8c23fb9ad280ed8
Michela Para <michela.para@meloni.sm>
2019-06-18 09:56:04 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Michela on 18/06/19.
+//  Copyright © 2019 Meloni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
